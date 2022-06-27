aba7eae51d91047b7afaab2137f992d439db6863
Tiago Garbi Baganha <tgbmateriais@gmail.com>
2022-06-27 12:07:22 -0300

    criando a class race

--- src/Races/Race.ts
+++ src/Races/Race.ts
@@ -1,19 +1,29 @@
 export class Race {
-  name: string;
-  dexterity: number;
-  constructor(name: string, dexterity: number) {
-    this.name = name;
-    this.dexterity = dexterity;
-  }
-  get nameInfo () {
-    return this.name;
-  }
-  get dexterityInfo () {
-    return this.dexterity;
-  }
-  createdRacesInstances(): number {
+  private readonly _name: string;
+  private readonly _dexterity: number;
 
+  constructor(
+    name: string,
+    dexterity: number,
+  ) {
+    this._name = name;
+    this._dexterity = dexterity;
+    this.instanceNumber = 0;
   }
+
+  get name () {
+    return this._name;
+  }
+
+  get dexterity () {
+    return this._dexterity;
+  }
+
+  static createdRacesInstances(): number {
+    this.instanceNumber += 1;
+    return this.instanceNumber;
+  }
+
   get maxLifePoints () {
     
   }
